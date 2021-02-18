1659a3b65dfb10ed54c8b5f882feb8518eb8ded5
pj_geniltonpaula <pj_geniltonpaula@bocombbm.com.br>
2021-02-18 10:35:21 -0300

    first commit

---
+++ Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Aguardando mensagens...");
+
+            var factory = new ConnectionFactory()
+            {
+                Uri = new Uri("amqp://guest:guest@localhost:5672/")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+
+            channel.QueueDeclare(queue: "Fila_01",
+                                durable: true,
+                                exclusive: false,
+                                autoDelete: false,
+                                arguments: null);
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (object sender, BasicDeliverEventArgs e) => {
+
+                int timeSleep = new Random().Next(10, 1500);
+                Console.WriteLine(
+                    $"[Nova mensagem | {timeSleep} | {DateTime.Now:yyyy-MM-dd HH:mm:ss}] " +
+                    Encoding.UTF8.GetString(e.Body.ToArray()));
+
+                Thread.Sleep(timeSleep);
+                channel.BasicAck(e.DeliveryTag, false);
+            };
+
+            channel.BasicConsume(
+                queue: "Fila_01",
+                autoAck: false,
+                consumer: consumer);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Producer/Program.cs
@@ -0,0 +1,47 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testando o envio de mensagens para uma Fila do RabbitMQ");
+
+            string connectionString = "amqp://guest:guest@localhost:5672/";
+            string queueName = "Fila_01";
+
+            try
+            {
+                var factory = new ConnectionFactory()
+                {
+                    Uri = new Uri(connectionString)
+                };
+                using var connection = factory.CreateConnection();
+                using var channel = connection.CreateModel();
+
+                channel.QueueDeclare(queue: queueName,
+                                        durable: true,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                for (int i = 0; i < 100000; i++)
+                {
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: queueName,
+                                         basicProperties: null,
+                                         body: Encoding.UTF8.GetBytes($"Mensagem {i}"));
+                    Console.WriteLine($"[Mensagem enviada] {i}");
+                }
+                Console.WriteLine("Concluido o envio de mensagens");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Exceção: {ex.GetType().FullName} | " + $"Mensagem: {ex.Message}");
+            }
+        }
+    }
+}
